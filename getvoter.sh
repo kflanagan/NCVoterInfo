052f5014922ff77aeae9585c8cf4aae7c8e380a6
Kevin Flanagan <flanagannc@gmail.com>
2017-04-16 18:18:46 -0400

    Add existing scripts

---
+++ getvoter.sh
@@ -0,0 +1,6 @@
+# wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter92.zip
+# wget http://msweb03.co.wake.nc.us/bordelec/downloads/data/vrdb.zip
+# wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+# wget https://s3.amazonaws.com/dl.ncsbe.gov/Campaign_Finance/Database.zip
+
+# http://dl.ncsbe.gov/index.html?prefix=data/
