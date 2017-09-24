c6ec31e9257a321b90f42932f310bcc00f4bfa53
Kevin Flanagan <flanagannc@gmail.com>
2017-09-23 21:01:11 -0400

    Add fetching files for initial load

--- CreateNCVoters.sh
+++ CreateNCVoters.sh
@@ -8,4 +8,28 @@ echo "it requires MySQL, wget and a database user root with full DBA rights"
 echo "on the MySQL instance"
 echo " "
 echo " "
+
+TXT=ncvoter_Statewide.txt
+if [ -f $TXT ]; then
+   rm $TXT
+fi
+
+
+ZIP=ncvoter_Statewide.zip
+if [ -f $ZIP ]; then
+   rm $ZIP
+   wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+
+else
+   wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+fi
+
+ if [ -f $ZIP ]; then
+   unzip $ZIP
+
+fi
+
+
+
+
 mysql -u kevin -p < CreateNCVoters.sql
