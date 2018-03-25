62c6fa568d4a5be952192c200469152592b764fa
user <user@Dell390.flanagannc.net>
2018-03-25 18:37:26 -0400

    Bring branch up to date

--- CreateNCVoters.sh
+++ CreateNCVoters.sh
@@ -1,1 +1,36 @@
+echo " "
+echo "This script will create a new schema and table, then it will create "
+echo "a user named user, with a password of password."
+echo " "
+echo "When that is finished it will download the entire state voter" 
+echo "registration database and load it into that table"
+echo "it requires MySQL, wget and a database user root with full DBA rights"
+echo "on the MySQL instance"
+echo " "
+echo " "
+
+TXT=ncvoter_Statewide.txt
+if [ -f $TXT ]; then
+   rm $TXT
+fi
+
+
+ZIP=ncvoter_Statewide.zip
+if [ -f $ZIP ]; then
+   rm $ZIP
+   wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+
+else
+   wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+fi
+
+ if [ -f $ZIP ]; then
+   unzip $ZIP
+
+fi
+
+USER=root
+
+echo "This uses the user id of $USER"
+
 mysql -u root -p < CreateNCVoters.sql
--- reloadNC-Voters.sql
+++ reloadNC-Voters.sql
@@ -1,4 +1,6 @@
 use NCVoters;
 truncate table voters;
 LOAD DATA local INFILE 'ncvoter_Statewide.txt' INTO TABLE voters FIELDS TERMINATED BY '	' enclosed by '"'  IGNORE 1 LINES ;
+CREATE USER IF NOT Exists 'user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON NCVoters.voters TO 'user'@'localhost';
 select * from voters limit 20
