068c84c3ae10747bce9d9dedf2c15d55269b09c5
voters <voters@centos.flanagannc.net>
2018-04-06 19:51:04 -0400

    Simplified sql scripts

--- reloadNC-Voters.sql
+++ reloadNC-Voters.sql
@@ -1,6 +1,5 @@
 use NCVoters;
 truncate table voters;
 LOAD DATA local INFILE 'ncvoter_Statewide.txt' INTO TABLE voters FIELDS TERMINATED BY '	' enclosed by '"'  IGNORE 1 LINES ;
-CREATE USER IF NOT Exists 'user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON NCVoters.voters TO 'user'@'localhost';
+select count(*) from voters
 select * from voters limit 20
