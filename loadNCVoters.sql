27560da0921f15e80483433a80ebf3f504c942d1
Kevin Flanagan <flanagannc@gmail.com>
2019-04-21 18:05:02 -0400

    Fixed table name

--- loadNCVoters.sql
+++ loadNCVoters.sql
@@ -2,4 +2,4 @@ use NCVoters;
 SET GLOBAL local_infile = 1;
 set global sql_mode = '';
 LOAD DATA local INFILE './ncvoter_Statewide.txt' INTO TABLE RegVoters FIELDS enclosed by '"'  IGNORE 1 LINES ;
-select count(*) from voters;
+select count(*) from RegVoters;
