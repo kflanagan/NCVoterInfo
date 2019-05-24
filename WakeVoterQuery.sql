00c74b4d7033b8a41fb3adeee3f5118d1e90f41b
Kevin Flanagan <flanagannc@gmail.com>
2019-05-24 06:40:26 -0400

    Just tuning the query

--- WakeVoterQuery.sql
+++ WakeVoterQuery.sql
@@ -1,15 +1,17 @@
 use NCVoters;
-SELECT first_name as 'First Name', last_name as 'Last Name', mail_addr1 as 'Street',
-w.voter_reg_num as 'Registration', age AS 'Birth year', birth_state AS 'Birth State' ,
-election_desc as 'Election Date', voting_method AS 'Voting method' , voted_party_desc AS 'Party',
-vtd_label AS 'Precienct',
-registr_dt as 'Registration Date'
+SELECT r.first_name as 'First Name', r.last_name as 'Last Name', mail_addr1 as 'Street',
+r.voter_reg_num as 'Registration' 
 
-from VoterHistoryWake w
-Left JOIN RegVoters r
-on w.voter_reg_num = r.voter_reg_num
+-- from VoterHistoryWake w
+-- Left JOIN RegVoters r
+-- on w.voter_reg_num = r.voter_reg_num
+
+from RegVoters r
+Left JOIN VoterHistoryWake w
+on r.voter_reg_num = w.voter_reg_num 
+
 
 where 1 = 1
-and first_name like '%David%'
-and last_name LIKE 'Cox%'
-AND birth_state NOT LIKE '%nc%'
+and first_name like '%Kevin%'
+and last_name LIKE 'Flanagan%'
+-- AND birth_place NOT LIKE '%nc%'
