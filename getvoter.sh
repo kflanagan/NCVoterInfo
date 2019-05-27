5bd747ab93151af321588854e9ff0381bb7f8a8d
Kevin Flanagan <flanagannc@gmail.com>
2019-05-27 12:27:59 -0400

    All good

--- getvoter.sh
+++ getvoter.sh
@@ -1,8 +1,10 @@
-wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter92.zip
-unzip ncvoter92.zip
+# wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter92.zip
+# unzip ncvoter92.zip
+# wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis92.zip
 # wget http://msweb03.co.wake.nc.us/bordelec/downloads/data/vrdb.zip
 wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvoter_Statewide.zip
+#wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis_Statewide.zip
 # wget https://s3.amazonaws.com/dl.ncsbe.gov/Campaign_Finance/Database.zip
-
+wget https://s3.amazonaws.com/dl.ncsbe.gov/data/ncvhis92.zip
 # http://dl.ncsbe.gov/index.html?prefix=data/
 unzip ncvoter_Statewide.zip
--- loadNCVotersWake.sql
+++ loadNCVotersWake.sql
@@ -1,5 +1,5 @@
 use NCVoters;
 SET GLOBAL local_infile = 1;
 set global sql_mode = '';
-LOAD DATA local INFILE './ncvhis92.txt' INTO TABLE VoterHistoryWake FIELDS enclosed by '"'  IGNORE 1 LINES ;
+LOAD DATA local INFILE './ncvoter92.txt' INTO TABLE VoterHistoryWake FIELDS enclosed by '"'  IGNORE 1 LINES ;
 select count(*) from voters;
